9992a0d4f004494e3b0ccfe05d1ddb2b57c78616
Catalina <catagonzalezaraujo@gmail.com>
2025-09-15 15:53:37 -0300

    Guia - Punteros

--- Guias/Guia - Punteros/ej11.c
+++ Guias/Guia - Punteros/ej11.c
@@ -7,3 +7,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void copiarCadena(char *cadena1, const char *cadena2) { 
+    /* --> Uso 'char' cuando voy a modificar el contenido de la cadena.
+       --> Uso 'const char' cuando no voy a modificar la cadena, solo leerla */
+    while (*cadena2 != '\0') {
+        *cadena1 = *cadena2;
+        *cadena1++;
+        *cadena2++;
+    }
+    
+    *cadena1 = '\0'; /* Me aseguro que sea el fin de la cadena */
+}
+
+int main(void) {
+    char cadena1[100] = "Hola Mundo";
+    const char *cadena2 = "Nuevo Hola Mundo";
+    
+    printf("\nCadenas antes de la copia...\n");
+    printf("Cadena 1: %s\n", cadena1);
+    printf("Cadena 2: %s\n", cadena2);
+
+    copiarCadena(cadena1, cadena2);
+
+    printf("\nCadenas despues de la copia...\n");
+    printf("Cadena 1: %s\n", cadena1);
+    printf("Cadena 2: %s\n", cadena2);
+
+    return 0;
+}
--- Guias/Guia - Punteros/ej13.c
+++ Guias/Guia - Punteros/ej13.c
@@ -1,11 +1,41 @@
 /* Escriba una funcion que recib un puntero a una cadena de caracteres y un caracter, 
    y devuelva la posicion (indice) de la primera ocurrencia del caracter en la cadena. 
+   Si el caracter no se encuentra en la cadena, la funcion debe devolver un -1.
    Luego, llame a esta funcion desde main y muestre el resultado. 
 */
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h> /* Libreria para usar 'tolower' */
+
+int buscarCaracter(const char *cadena, char caracter) {
+    int indice = 0;
+    char buscado = tolower(caracter);
+
+    /* La funcion 'tolower' convierte cualquier letra a su version minuscula */
+
+    while (cadena[indice] != '\0') {
+        if (tolower(cadena[indice]) == buscado) {
+            return indice;
+        }
+        indice++;
+    }
+
+    return -1; /* No se encontro ese caracter en la cadena */
+}
 
 
+int main(void) {
+    const char *cadena = "Hola Mundo";
+    char caracter = 'm';
 
+    int encontrado = buscarCaracter(cadena, caracter);
 
+    if (encontrado != -1) {
+        printf("\nEl caracter '%c' se encuentra en la posicion %d.\n", caracter, encontrado);
+    } else {
+        printf("\nEl caracter '%c' no se encuentra en la cadena.\n", caracter);
+    }
+
+    return 0;
+}
